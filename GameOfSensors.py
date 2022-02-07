434fa524babdcc6240e8c084624036bf5cd0aad7
Joel Reyes <reyesvargasjoel@gmail.com>
2022-02-07 15:27:42 -0600

    Se agregan las condiciones de simulación establecidas en EERP

--- GameOfSensors.py
+++ GameOfSensors.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+if __name__ == '__main__':
+    areaX = 200
+    areaY = 200
+    nSensors = 50
+    txRange = 80
+    sinkX = 200
+    sinkY = 200
+    initialEnergy = 5
+    eElec = 50e-09
+    eAmp = 100e-12
+    pcktSize = 500*8
+
 class Sensor:
     def __init__(self, task, x, y):
         self.task = task
@@ -12,3 +24,4 @@ class Sensor:
         self.vecindad = []
         self.tiempoActivo = 0
         self.tiempoInactivo = 0
+        self.bateria = initialEnergy
